25ec402aa6fb33e116fc5474f7077c3269e426f6
Susil25102007 <143895859+Susil25102007@users.noreply.github.com>
2024-01-21 16:47:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/test1.git /LazyPrincess
+  git clone https://github.com/Susil25102007/Susil.git /Susil
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /Susil
 fi
 cd /LazyPrincess
 pip3 install -U -r requirements.txt
